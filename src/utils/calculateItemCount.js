74ab8bd69b7e11645b74e0b2f58b0278b8fd1779
Alena <olena.yarmak.mail@gmail.com>
2025-05-22 22:10:18 +0200

    add calculateItemCount / add getRandomNumber in utils

---
+++ src/utils/calculateItemCount.js
@@ -0,0 +1,3 @@
+export const calculateItemCount = (x, y) => {
+    return x * y
+}
---
+++ src/utils/getRandomNumber.js
@@ -0,0 +1,4 @@
+export const getRandomNumber = (num) => {
+    let randomNumber = Math.floor(Math.random() * num)
+    return randomNumber
+}
